778e651a90e27dfeed20768e5ac8bb69ffcffe3d
CatalinDoaga <118671748+CatalinDoaga@users.noreply.github.com>
2022-11-20 15:52:43 +0200

    Add files via upload

---
+++ magazin online/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "LAN Turtle",
+    price: 45,
+    desc: "LAN Turtle este un instrument de administrare a sistemelor si de testare.",
+    img: "images/img-1.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "USB Rubber Ducky",
+    price: 100,
+    desc: "USB Rubber Ducky - care arata ca o unitate flash inocenta pentru oameni.",
+    img: "images/img-2.webp",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Wifi PINEAPPLE",
+    price: 25,
+    desc: "Noul WiFi Pineapple Mark VII ofera performante incredibile dintr-o interfata web simpla.",
+    img: "images/img-3.webp",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Bash Bunny",
+    price: 119,
+    desc: "Platforma de încărcare inovatoare care a introdus atacuri USB multi-vector a evoluat.",
+    img: "images/img-4.webp",
+  },
+];
